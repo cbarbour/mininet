4f6e394552e5cc4161eb47f60658d270938ff519
Chris Barbour <chris_barbour@apple.com>
2016-10-05 00:20:49 -0700

    Initial commit of Packer AMI creation templates
    This automation can quickly and easily build AMI images. This will
    hopfully encourage more frequent updates to Amazon.

---
+++ util/packer/README.md
@@ -0,0 +1,31 @@
+# Packer templates for Mininet
+
+## Overview
+
+These [Packer](https://www.packer.io) templates can be used to quickly and easily build AMIs for use in AWS.
+
+## Usage
+
+In most cases, an AMI can be built simply by invoking `build.sh`. Build supplies a sane set of arguments to Packer.
+
+The currently checked out branch will be copied to the AMI, and the mininet installer will be invoked automatically. In order to build an AMI with a specific version of Mininet, first check out the appropriate branch or tag of the mininet repository.
+
+In order to build an AMI, you will need to have configured your AWS authentication credentials, and you must have approprivate priveleges within the account.
+
+See the AWS Command Line Interface documentation for additional information on [how to configure access credentials](http://docs.aws.amazon.com/cli/latest/userguide/cli-chap-getting-started.html#cli-config-files).
+
+## Advanced usage
+
+The `build.sh` script will pass any supplied arguments directly to packer. You can use this to pass account specific settings to the build script that you might not want to hard code into your configuration.
+
+For example, if your account does not have a default VPC, you may pass your VPC ID and Subnet ID like this:
+
+```
+./build.sh -var 'vpc_id=vpc-1234abcd' -var 'subnet_id=subnet-567890ef'
+```
+
+## Issues / Enhancements
+
+Currently, these templates only build AMIs. It would be fairly straightforward to support other providers or platforms in the future. The Mininet repository includes seperate automation for VM creation.
+
+The template does not provide a way to build a VM without a full local copy of the repository. The actual installer automation scripts do offer this ability.
---
+++ util/packer/files/install-mininet-vm.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+REMOTE="git://github.com/mininet/mininet"
+DIR="/home/mininet/mininet"
+REF=""
+
+while getopts ":d:r:c:h" opt; do
+  case $opt in
+    r) REMOTE=${OPTARG} ;;
+    d) DIR=${OPTARG} ;;
+    c) REF=${OPTARG} ;;
+    h)
+      help
+      exit 0
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      echo >&2
+      help >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      echo >&2
+      help >&2
+      exit 1
+      ;;
+  esac
+done
+
+help() {
+  echo "Usage: `basename $0` options [-r remote] [-d repository_dir] [-c checkout_ref] [-h]"
+}
+
+
+if [ "$(id -nu)" -ne 'mininet']; then
+  if ! id mininet; then
+    sudo useradd --comment 'mininet' --user-group --create-home --shell='/bin/bash' mininet
+  fi
+
+  grep "mininet" /etc/sudoers || \
+    echo "mininet ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers > /dev/null
+
+  chown -R mininet:mininet ~mininet/
+
+  # Run the rest of this script as the mininet user
+  exec sudo mininet "$0" "$@"
+fi
+
+# This script is intended to install Mininet into
+# a brand-new Ubuntu virtual machine,
+# to create a fully usable "tutorial" VM.
+#
+# optional argument: Mininet branch to install
+set -e
+sudo sed -i -e 's/Default/#Default/' /etc/sudoers
+echo mininet-vm | sudo tee /etc/hostname > /dev/null
+sudo sed -i -e 's/ubuntu/mininet-vm/g' /etc/hosts
+sudo hostname `cat /etc/hostname`
+sudo sed -i -e 's/splash//' /etc/default/grub
+sudo sed -i -e 's/quiet/text/' /etc/default/grub
+sudo update-grub
+# Update from official archive
+sudo apt-get update
+# 12.10 and earlier
+#sudo sed -i -e 's/us.archive.ubuntu.com/mirrors.kernel.org/' \
+#	/etc/apt/sources.list
+# 13.04 and later
+#sudo sed -i -e 's/\/archive.ubuntu.com/\/mirrors.kernel.org/' \
+#	/etc/apt/sources.list
+# Clean up vmware easy install junk if present
+if [ -e /etc/issue.backup ]; then
+    sudo mv /etc/issue.backup /etc/issue
+fi
+if [ -e /etc/rc.local.backup ]; then
+    sudo mv /etc/rc.local.backup /etc/rc.local
+fi
+sudo apt-get -y install git-core openssh-server
+
+if ! git -C "${DIR}" status 2>/dev/null 2>&1; then
+  # Fetch Mininet
+  git clone "${REMOTE}" "${DIR}"
+fi
+
+# Optionally check out branch
+if [ -z "${REF}" ]; then
+  # Repository will in detached HEAD state if REF
+  # is anything but a branch. This is fine unless
+  # you want to commit changes.
+  git -C "${DIR}" checkout "${REF}"
+fi
+
+# Install Mininet
+time "${DIR}/util/install.sh"
+# Finalize VM
+time "${DIR}/util/install.sh" -tcd
+# Ignoring this since NOX classic is deprecated
+#if ! grep NOX_CORE_DIR .bashrc; then
+#  echo "export NOX_CORE_DIR=~/noxcore/build/src/" >> .bashrc
+#fi
+echo "Done preparing Mininet VM."
